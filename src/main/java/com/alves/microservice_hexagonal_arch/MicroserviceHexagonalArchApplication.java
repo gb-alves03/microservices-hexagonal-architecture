06c0bd8aeb6777a60e391f8455efa198e5c8dc88
Gabriel Alves <gabrielhalves03@gmail.com>
2025-08-30 11:27:33 -0300

    feat: add dependencies and other application configurations

--- src/main/java/com/alves/microservice_hexagonal_arch/MicroserviceHexagonalArchApplication.java
+++ src/main/java/com/alves/microservice_hexagonal_arch/MicroserviceHexagonalArchApplication.java
@@ -2,7 +2,9 @@ package com.alves.microservice_hexagonal_arch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MicroserviceHexagonalArchApplication {
 
