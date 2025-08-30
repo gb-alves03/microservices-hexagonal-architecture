497520b9b9ff91dca6a433957a97a75b04018025
Gabriel Alves <gabrielhalves03@gmail.com>
2025-08-30 09:51:10 -0300

    feat: initial commit

---
+++ src/main/java/com/alves/microservice_hexagonal_arch/MicroserviceHexagonalArchApplication.java
@@ -0,0 +1,13 @@
+package com.alves.microservice_hexagonal_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceHexagonalArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceHexagonalArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alves/microservice_hexagonal_arch/MicroserviceHexagonalArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.microservice_hexagonal_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceHexagonalArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
